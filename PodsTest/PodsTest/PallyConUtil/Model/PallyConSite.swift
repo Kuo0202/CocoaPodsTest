f4eb705cc14df8a1b78716fa6017e8f519af2de5
PeterKuo <peter.kuo@vascreative.com>
2021-10-26 18:28:26 +0800

    add basic

---
+++ PodsTest/PodsTest/PallyConUtil/Model/PallyConSite.swift
@@ -0,0 +1,15 @@
+//
+//  PallyConSite.swift
+//  DrmSDK_iOS
+//
+//  Created by Peter Kuo on 2021/10/25.
+//
+
+import Foundation
+
+struct PallyConSite: Codable {
+    var siteId: String
+    var siteKey: String
+    var accessKey: String
+    
+}
